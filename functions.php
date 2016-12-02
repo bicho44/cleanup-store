ea001af9e18c1649e490a1770872e0558cd277e6
Federico <admin@imgdigital.com.ar>
2016-12-02 10:06:21 -0300

    Orden Alfabético

--- functions.php
+++ functions.php
@@ -17,3 +17,23 @@ function enqueue_parent_theme_style() {
  * Load IMGD Framework compatibility file.
  */
 //require get_template_directory() . '/imgd/imgd_funciones.php';
+add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
+
+function custom_woocommerce_get_catalog_ordering_args( $args ) {
+    $orderby_value = isset( $_GET['orderby'] ) ? woocommerce_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+
+    if ( 'alphabetical' == $orderby_value ) {
+        $args['orderby'] = 'title';
+        $args['order'] = 'DESC';
+    }
+
+    return $args;
+}
+
+add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
+add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
+
+function custom_woocommerce_catalog_orderby( $sortby ) {
+    $sortby['alphabetical'] = __( 'Alphabetical' );
+    return $sortby;
+}
